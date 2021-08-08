b297e375200c00edc747b45437f4d8f52b297f11
Pierce <pierce@vulpes.dev>
2021-08-08 00:00:41 -0400

    initial

---
+++ lua/autorun/server/toggle_voice_init.lua
@@ -0,0 +1,43 @@
+local forceLocalVoiceConvar = CreateConVar( "force_local_voice", "0", 0, "force everyone to use local voice")
+forceLocalVoice = forceLocalVoiceConvar:GetBool()
+
+cvars.AddChangeCallback("force_local_voice", function(convarName, valueOld, valueNew)
+	foreLocalVoice = forceLocalVoiceConvar:GetBool()
+end)
+
+local config = {
+	CHAT_DISTANCE = 750,
+	VOICE_3D = true
+}
+
+local playerConfig = {}
+
+local function canHear( listener, speaker )
+    if not listener:Alive() or not speaker:Alive() then
+        return false
+    end
+
+    local speakerPos = speaker:GetPos()
+    local listenerPos = listener:GetPos()
+
+    if listenerPos:DistToSqr( speakerPos ) > GAMEMODE.Config.Chat.CHAT_DISTANCE ^ 2 then
+        return false
+    end
+
+    return true
+end
+
+hook.Add( "PlayerCanHearPlayersVoice", "CFC_ToggleLocalVoice_CanHear", function( listener, speaker )
+	if not forceLocalVoice and not playerConfig[listener] then return end
+
+    return canHear( listener, speaker ), config.Chat.VOICE_3D
+end)
+
+
+concommand.Add("enable_local_voice", function(ply)
+	playerConfig[ply] = true
+end)
+
+concommand.Add("disable_local_voice", function(ply)
+	playerConfig[ply] = nil
+end)
