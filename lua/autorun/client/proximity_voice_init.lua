55b1b0e7a2565caa8822527316ecfad8376b3f5b
Pierce <pierce@vulpes.dev>
2021-08-14 13:42:37 -0400

    format  ulx command add button to options menu

--- lua/autorun/client/proximity_voice_init.lua
+++ lua/autorun/client/proximity_voice_init.lua
@@ -1,6 +1,22 @@
-CreateClientConVar( "proximity_voice_enabled", "0", true, true )
+local convar = CreateClientConVar( "proximity_voice_enabled", "0", true, true )
 cvars.AddChangeCallback( "proximity_voice_enabled", function( name, old, new )
 	net.Start( "proximity_voice_enabled_changed" )
 		net.WriteBool( tobool(new) )
 	net.SendToServer()
 end)
+
+
+local function populatePanel( form )
+	local checkbox = form:CheckBox( "Enable Proximity Voice", "proximity_voice_enabled" )
+  checkbox:SetChecked( convar:GetBool() )
+end
+
+hook.Add( "AddToolMenuCategories", "CFC_OptionalAddons_AddMenuCategory", function()
+  spawnmenu.AddToolCategory( "Options", "CFC", "Proximity Voice" )
+end )
+
+hook.Add( "PopulateToolMenu", "CFC_OptionalAddons_CreateOptionsMenu", function()
+  spawnmenu.AddToolMenuOption( "Options", "CFC", "proximity_voice", "Proximity Voice", "", "", function( panel )
+	  populatePanel( panel )
+  end )
+end )
--- lua/ulx/modules/sh/proximity_voice_commands.lua
+++ lua/ulx/modules/sh/proximity_voice_commands.lua
@@ -1,11 +1,11 @@
 local function forceProximityVoice( caller, disabled )
-	if disabled then
-		RunConsoleCommand("force_proximity_voice", "0")
-		ulx.fancyLogAdmin(  caller, "#A disabled proximity voice globally" )
-	else
-		RunConsoleCommand("force_proximity_voice", "1")
-		ulx.fancyLogAdmin(  caller, "#A enabled proximity voice globally")
-	end
+    if disabled then
+        RunConsoleCommand("force_proximity_voice", "0")
+        ulx.fancyLogAdmin(  caller, "#A disabled proximity voice globally" )
+    else
+        RunConsoleCommand("force_proximity_voice", "1")
+        ulx.fancyLogAdmin(  caller, "#A enabled proximity voice globally")
+    end
 end
 local proximityGlobal = ulx.command( CATEGORY_NAME, "ulx enableproximityvoice", forceProximityVoice )
 proximityGlobal:addParam{ type=ULib.cmds.BoolArg, invisible=true }
