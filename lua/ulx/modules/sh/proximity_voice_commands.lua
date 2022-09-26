a214ae5e42ade7ef8f3f431a65556fa94af99713
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-09-26 09:54:57 -0700

    Fix linting errors

--- lua/ulx/modules/sh/proximity_voice_commands.lua
+++ lua/ulx/modules/sh/proximity_voice_commands.lua
@@ -1,17 +1,17 @@
 local function forceProximityVoice( caller, disabled )
     if disabled then
-        RunConsoleCommand("force_proximity_voice", "0")
+        RunConsoleCommand( "force_proximity_voice", "0" )
         ulx.fancyLogAdmin(  caller, "#A disabled proximity voice globally" )
     else
-        RunConsoleCommand("force_proximity_voice", "1")
-        ulx.fancyLogAdmin(  caller, "#A enabled proximity voice globally")
+        RunConsoleCommand( "force_proximity_voice", "1" )
+        ulx.fancyLogAdmin(  caller, "#A enabled proximity voice globally" )
     end
 end
 local proximityGlobal = ulx.command( CATEGORY_NAME, "ulx enableproximityvoice", forceProximityVoice )
 proximityGlobal:addParam{ type = ULib.cmds.BoolArg, invisible = true }
 proximityGlobal:defaultAccess( ULib.ACCESS_ADMIN )
 proximityGlobal:help( "Enable proximity voice for everyone" )
-proximityGlobal:setOpposite( "ulx disableproximityvoice", {nil, true} )
+proximityGlobal:setOpposite( "ulx disableproximityvoice", { nil, true } )
 
 
 local function proximityVoiceOverride( caller, targets, enabled )
