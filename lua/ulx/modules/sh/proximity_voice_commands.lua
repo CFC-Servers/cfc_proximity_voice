e6497cfa9109766d25bb53eb84de77f32e01c949
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2026-03-22 14:56:29 -0700

    Replace overrideproximityvoice with timedproxyvoice

--- lua/ulx/modules/sh/proximity_voice_commands.lua
+++ lua/ulx/modules/sh/proximity_voice_commands.lua
@@ -1,3 +1,5 @@
+local CATEGORY_NAME = "Fun"
+
 local function forceProximityVoice( caller, disabled )
     if disabled then
         RunConsoleCommand( "force_proximity_voice", "0" )
@@ -14,16 +16,23 @@ proximityGlobal:help( "Enable proximity voice for everyone" )
 proximityGlobal:setOpposite( "ulx disableproximityvoice", { nil, true } )
 
 
-local function proximityVoiceOverride( caller, targets, enabled )
-    for _, ply in ipairs( targets ) do
-        ProximityVoiceOverridePlayerConfig( ply, enabled )
+if not TimedPunishments then return end
+
+local PUNISHMENT = "timedproxyvoice"
+local HELP = "Force proximity voice on a player for a set duration."
+
+if SERVER then
+    local function enable( ply )
+        ProximityVoiceOverridePlayerConfig( ply, true )
     end
 
-    local word = enabled and "enabled" or "disabled"
-    ulx.fancyLogAdmin( caller, "#A #s proximity voice for #T", word, targets )
+    local function disable( ply )
+        ProximityVoiceOverridePlayerConfig( ply, false )
+    end
+
+    TimedPunishments.Register( PUNISHMENT, enable, disable )
 end
-local proximityOverride = ulx.command( CATEGORY_NAME, "ulx overrideproximityvoice", proximityVoiceOverride )
-proximityOverride:addParam{ type = ULib.cmds.PlayersArg }
-proximityOverride:addParam{ type = ULib.cmds.BoolArg }
-proximityOverride:defaultAccess( ULib.ACCESS_ADMIN )
-proximityOverride:help( "Enable proximity voice for specific players" )
+
+local action = "forced proximity voice upon ##"
+local inverseAction = "freed ## from proximity voice"
+TimedPunishments.MakeULXCommands( PUNISHMENT, action, inverseAction, CATEGORY_NAME, HELP )
